e5b3443579e6758fe91892eb037fc672a805fabd
Kalem Mentore <kalem@Apples-MacBook-Air.local>
2023-04-27 14:56:09 -0400

    added footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
-}
+    <div className="footer py-5 text-center">
+      <Container>
+        <div className="footer-text">
+          <h3 className="fs-4 font-monospace">
+            Professional Security Services
+          </h3>
+          <span className="fs-1">contact us</span>
+          <div className="square my-3 mb-5" />
 
-export default Footer
+          <span className="footer-btn me-3">Contact Info</span>
+          <span className="footer-btn">Service Inquiry</span>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
