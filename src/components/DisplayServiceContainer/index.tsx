1faa0a846329066314e78d6123e44ee1970f4801
Kalem Mentore <kalem@Apples-MacBook-Air.local>
2023-04-28 09:27:40 -0400

    added animations

--- src/components/DisplayServiceContainer/index.tsx
+++ src/components/DisplayServiceContainer/index.tsx
@@ -1,6 +1,7 @@
 import { Row, Col, Container } from "react-bootstrap";
 import { DisplayServiceType } from "../../types";
 import "./DisplayService.css";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 interface Props {
   serviceObj: DisplayServiceType;
@@ -8,23 +9,30 @@ interface Props {
 
 const DisplayServiceContainer: React.FC<Props> = ({ serviceObj }) => {
   return (
-    <div className="py-5 cream-bg">
-      <Container>
-        <Row>
-          <Col md={6}>
-            <h3 className="mb-3 text-uppercase fs-2">{serviceObj.title}</h3>
-            <p>{serviceObj.description}</p>
-          </Col>
-          <Col md={6}>
-            <img
-              src={serviceObj.img_url}
-              className="display-service-img"
-              alt="example"
-            />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <AnimationOnScroll
+      animateIn="animate__fadeInLeft"
+      duration={1}
+      delay={100}
+      animateOnce={true}
+    >
+      <div className="py-5 cream-bg">
+        <Container>
+          <Row>
+            <Col md={6}>
+              <h3 className="mb-3 text-uppercase fs-2">{serviceObj.title}</h3>
+              <p>{serviceObj.description}</p>
+            </Col>
+            <Col md={6}>
+              <img
+                src={serviceObj.img_url}
+                className="display-service-img"
+                alt="example"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </AnimationOnScroll>
   );
 };
 
--- src/components/LandingAboutUs/index.tsx
+++ src/components/LandingAboutUs/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { GoShield } from "react-icons/go";
 import { GiCctvCamera } from "react-icons/gi";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 import "./LandingAbout.css";
 
 const LandingPageAboutUs = () => {
@@ -12,36 +13,61 @@ const LandingPageAboutUs = () => {
           <Col xs={12} md={6}>
             <Row>
               <Col>
-                <div>
-                  <GoShield size={60} />
-                  <span className="blue-color fs-3 fw-bold mt-5"> 10+ </span>
-                </div>
-                <p className="fs-5 fw-bold dark-color mt-2">YEARS EXPERIENCE</p>
+                <AnimationOnScroll
+                  animateIn="animate__fadeInUp"
+                  delay={100}
+                  duration={1}
+                  animateOnce={true}
+                >
+                  <div>
+                    <GoShield size={60} />
+                    <span className="blue-color fs-3 fw-bold mt-5"> 10+ </span>
+                  </div>
+                  <p className="fs-5 fw-bold dark-color mt-2">
+                    YEARS EXPERIENCE
+                  </p>
 
-                <hr className="landing-line" />
+                  <hr className="landing-line" />
+                </AnimationOnScroll>
               </Col>
               <Col>
-                <div>
-                  <GiCctvCamera size={60} />
-                  <span className="blue-color fs-3 fw-bold mt-5"> 200+ </span>
-                </div>
-                <p className="fs-5 fw-bold dark-color mt-2">HAPPY CUSTOMERS</p>
+                <AnimationOnScroll
+                  animateIn="animate__fadeInUp"
+                  delay={350}
+                  duration={1}
+                  animateOnce={true}
+                >
+                  <div>
+                    <GiCctvCamera size={60} />
+                    <span className="blue-color fs-3 fw-bold mt-5"> 200+ </span>
+                  </div>
+                  <p className="fs-5 fw-bold dark-color mt-2">
+                    HAPPY CUSTOMERS
+                  </p>
 
-                <hr className="landing-line" />
+                  <hr className="landing-line" />
+                </AnimationOnScroll>
               </Col>
             </Row>
           </Col>
           <Col xs={12} md={6}>
-            <div className="dark-color">
-              <h3 className="fs-2 fw-bold mb-4">Welcome To Our Company</h3>
-              <p>
-                Fortress Security Services Limited is an innovative,
-                professional medium sized security firm with demonstrated
-                experience. We provide high caliber security personnel who have
-                been extensively trained using practical techniques based on
-                modern anticrime strategies.
-              </p>
-            </div>
+            <AnimationOnScroll
+              animateIn="animate__fadeInUp"
+              delay={550}
+              duration={1}
+              animateOnce={true}
+            >
+              <div className="dark-color">
+                <h3 className="fs-2 fw-bold mb-4">Welcome To Our Company</h3>
+                <p>
+                  Fortress Security Services Limited is an innovative,
+                  professional medium sized security firm with demonstrated
+                  experience. We provide high caliber security personnel who
+                  have been extensively trained using practical techniques based
+                  on modern anticrime strategies.
+                </p>
+              </div>
+            </AnimationOnScroll>
           </Col>
         </Row>
       </Container>
