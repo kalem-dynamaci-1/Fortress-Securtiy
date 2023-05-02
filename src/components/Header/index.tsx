635487e60d2f58bb3e59d2e8c152e72dc7a18d02
kalem-dynamaci-1 <kmentore@dynamic1group.com>
2023-05-02 10:29:17 -0400

    added styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Dropdown } from "react-bootstrap";
 import "./Header.css";
+import servicesList from "../ServicePageComponents/ServiceHelper";
 
 const Header = () => {
   const [active, setActive] = useState(false);
@@ -33,65 +34,17 @@ const Header = () => {
                   <span className="nav-link header-link">See All</span>
                 </Link>
               </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/protection-officers">
-                  <span className="nav-link header-link">
-                    Protection Officers
-                  </span>
-                </Link>
-              </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/personal-protection">
-                  <span className="nav-link header-link">
-                    {" "}
-                    Personal Protection
-                  </span>
-                </Link>{" "}
-              </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/cash-in-transit">
-                  <span className="nav-link header-link">
-                    {" "}
-                    Cash In Transit Services
-                  </span>
-                </Link>{" "}
-              </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/cctv-and-alarms">
-                  <span className="nav-link header-link"> CCTV & Alarms</span>
-                </Link>{" "}
-              </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/alarms-monitoring">
-                  <span className="nav-link header-link">
-                    {" "}
-                    Alarm Monitoring
-                  </span>
-                </Link>{" "}
-              </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/courier-service">
-                  <span className="nav-link header-link"> Courier Service</span>
-                </Link>{" "}
-              </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/investigations">
-                  <span className="nav-link header-link"> Investigations</span>
-                </Link>{" "}
-              </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/special-event-security">
-                  <span className="nav-link header-link">
-                    {" "}
-                    Special Event Security
-                  </span>
-                </Link>{" "}
-              </Dropdown.Item>
-              <Dropdown.Item as={"div"}>
-                <Link to="/service/gps-services">
-                  <span className="nav-link header-link"> GPS Services</span>
-                </Link>{" "}
-              </Dropdown.Item>
+              {servicesList.map((service, index) => {
+                return (
+                  <Dropdown.Item key={index} as={"div"}>
+                    <Link to={`/services/${service.title}`}>
+                      <span className="nav-link header-link">
+                        {service.title}
+                      </span>
+                    </Link>
+                  </Dropdown.Item>
+                );
+              })}
             </Dropdown.Menu>
           </Dropdown>
 
